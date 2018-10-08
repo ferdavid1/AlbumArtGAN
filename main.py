fde0bc8a500331ae2ff9215ae60f4d28fc7d4579
Fernando Espinosa <fespinosa@ucdavis.edu>
2018-10-08 16:45:42 -0700

    small

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ parser.add_argument('--b2', type=float, default=0.999, help='adam: decay of firs
 parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
 parser.add_argument('--latent_dim', type=int, default=100, help='dimensionality of the latent space')
 parser.add_argument('--img_size', type=int, default=640, help='size of each image dimension')
-parser.add_argument('--channels', type=int, default=1, help='number of image channels')
+parser.add_argument('--channels', type=int, default=3, help='number of image channels')
 parser.add_argument('--sample_interval', type=int, default=400, help='interval between image sampling')
 opt = parser.parse_args()
 print(opt)
@@ -110,7 +110,8 @@ generator.apply(weights_init_normal)
 discriminator.apply(weights_init_normal)
 
 train_dataset = datasets.ImageFolder(root='images/', transform=transforms.Compose([transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]))
-
+for (t, a) in train_dataset:
+	print(t.shape)
 dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True)
 
 # Optimizers
