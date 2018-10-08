dc6ce4f49a232b748c4772e33fd302ee871a940e
Fernando Espinosa <fespinosa@ucdavis.edu>
2018-10-08 13:58:13 -0700

    small

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # AlbumArtGAN
 
-## Generative Adversarial Network in Pytorch for album art generation
+## Deep Convolutional Generative Adversarial Network in Pytorch for album art generation
 	- uses Spotify API to pull album art from 2k of my favorite records. 
--- main.py
+++ main.py
@@ -2,19 +2,14 @@ import argparse
 import os
 import numpy as np
 import math
-
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
-
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torch.autograd import Variable
-
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
-
-os.makedirs('images', exist_ok=True)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
@@ -24,7 +19,7 @@ parser.add_argument('--b1', type=float, default=0.5, help='adam: decay of first 
 parser.add_argument('--b2', type=float, default=0.999, help='adam: decay of first order momentum of gradient')
 parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
 parser.add_argument('--latent_dim', type=int, default=100, help='dimensionality of the latent space')
-parser.add_argument('--img_size', type=int, default=32, help='size of each image dimension')
+parser.add_argument('--img_size', type=int, default=640, help='size of each image dimension')
 parser.add_argument('--channels', type=int, default=1, help='number of image channels')
 parser.add_argument('--sample_interval', type=int, default=400, help='interval between image sampling')
 opt = parser.parse_args()
