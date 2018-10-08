eb8c224a803ac2686e87cb608f0416d6fbe75206
Fernando Espinosa <fespinosa@ucdavis.edu>
2018-10-08 10:57:45 -0700

    small

--- make_image_folder.py
+++ make_image_folder.py
@@ -4,7 +4,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 client_credentials_manager = SpotifyClientCredentials(client_id='2b211ac7629d41699c48e4c5a9098ee3', client_secret='1373301210f141a1a9b7a60c684c5e05')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-playlists = sp.user_playlists('spotify')
+playlists = sp.user_playlists(user='1284712885')
 while playlists:
     for i, playlist in enumerate(playlists['items']):
         print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
