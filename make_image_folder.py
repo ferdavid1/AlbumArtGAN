b3613818e2d6d29ec8c534cd4ba397b0ee83c57c
Fernando Espinosa <fespinosa@ucdavis.edu>
2018-10-08 10:51:32 -0700

    small

--- make_image_folder.py
+++ make_image_folder.py
@@ -1,7 +1,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_credentials_manager = SpotifyClientCredentials()
+client_credentials_manager = SpotifyClientCredentials(client_id='2b211ac7629d41699c48e4c5a9098ee3', client_secret='1373301210f141a1a9b7a60c684c5e05')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 playlists = sp.user_playlists('spotify')
