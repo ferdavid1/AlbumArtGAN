f40f6eec1e0289bfb28747d1f693af7f95435147
Fernando Espinosa <fespinosa@ucdavis.edu>
2018-10-13 02:51:23 -0700

    small

--- scale_results.py
+++ scale_results.py
@@ -8,7 +8,7 @@ def scale(filename, outfilename):
 	return img
 
 if __name__ == '__main__':
-	os.chdir('images/')
+	os.chdir('images/lr-0002') # change lr-0002 to whichever trial run you're on
 	for imgname in os.listdir('results/'):
 		scaled = scale('results/' + imgname, imgname.strip('.png') + '_scaled.png')
 		print("Scaled: ", imgname)
