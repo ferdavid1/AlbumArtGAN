8ad993f3a2c9e88730cc7a49fe7d4d47b5a4bd43
Fernando Espinosa <fespinosa@ucdavis.edu>
2018-10-14 08:26:55 -0700

    small

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 ## Deep Convolutional Generative Adversarial Network in Pytorch for album art generation
 	- uses Spotify API to pull album art from 2k of my favorite records. 
+
+## Album Art Contenders
+	- from lr = 0.001:
+		- 400_scaled.png
+		- 800_scaled.png
+		- 1200_scaled.png
+		- 2000_scaled.png
+		- 3200_scaled.png
+		- 3600_scaled.png
+		- 5200_scaled.png
+
+	- from lr = 0.0002:
+		- 1200_scaled.png
+		- 2000_scaled.png
--- scale_results.py
+++ scale_results.py
@@ -9,7 +9,7 @@ def scale(filename, outfilename):
 	contrast = ImageEnhance.Contrast(img) # enhance contrast
 	contrast.enhance(5).save('scaled_results/' + outfilename)
 if __name__ == '__main__':
-	os.chdir('images/lr-001') # change lr-00XXX to whichever trial run you're on
+	os.chdir('images/lr-0002') # change lr-00XXX to whichever trial run you're on
 	for imgname in os.listdir('results/'):
 		scaled = scale('results/' + imgname, imgname.strip('.png') + '_scaled.png')
 		print("Scaled: ", imgname)
